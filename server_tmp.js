20d2bb0e106f3108af5210262f5107c908de8db8
Qinfu <qinfug@gmail.com>
2020-01-14 20:11:29 +0900

    support command line arguments
    support command line arguments for configurating server protoco

--- server_tmp.js
+++ server_tmp.js
@@ -2,11 +2,19 @@ const trace = function(xMsg){
   console.log(">>" + xMsg);
 }
 
+
+let isHttps = false;
+
+//コマンドライン引数取得
+if(process.argv[2] && process.argv[2] == "true"){
+	isHttps = true;
+}
+
 //ベーシックサーバ作成
 let oHttpServer = require('./_asset/server_base');
 
 
-let _server = oHttpServer.run("3000","./_htdocs/",true);
+let _server = oHttpServer.run("3000","./_htdocs/",isHttps);
 
 _server.on("getPost", (err,xObject) =>{
 	trace("Get POST!");
